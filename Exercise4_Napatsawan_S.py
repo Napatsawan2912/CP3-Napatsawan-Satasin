ea96c1a3f2ceb003d05c8aacf432425cb491f95c
Napatsawan2912 <satasin5752@gmail.com>
2023-11-11 15:10:01 +0700

    exercise4
    Create a variable to keep score and show output

---
+++ Exercise4_Napatsawan_S.py
@@ -0,0 +1,10 @@
+Eng = 54.20
+Biz = 87.07
+Com_Sys = 91.06
+Com_Pro = 99.99
+
+print("--- Your Score ---")
+print("Foundation English : ", Eng)
+print("General Business : ", Biz)
+print("Introduction to Computer Systems : ", Com_Sys)
+print("Computer Programming : ", Com_Pro)
