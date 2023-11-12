419c9ba4a2941d99fe6a3cf9c719a384770b40f8
Napatsawan2912 <satasin5752@gmail.com>
2023-11-12 19:34:16 +0700

    exercise5.1

---
+++ Exercise5_1_Napatsawan_S.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter Number1: "))
+num2 = int(input("Enter Number2: "))
+print("--------------------")
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
