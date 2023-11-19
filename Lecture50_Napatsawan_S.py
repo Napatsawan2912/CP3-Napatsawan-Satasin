35266ef6f74b4d8c4e714f2ca2e6800197f2ab77
Napatsawan2912 <satasin5752@gmail.com>
2023-11-19 19:30:06 +0700

    lecture50

---
+++ Lecture50_Napatsawan_S.py
@@ -0,0 +1,16 @@
+def addNumber(x, y):
+    print(x + y)
+
+def deleteNumber(x, y):
+    print(x - y)
+
+def multiplyNumber(x, y):
+    print(x * y)
+    
+def divideNumber(x, y):
+    print(x / y)
+
+addNumber(1, 2) 
+deleteNumber(1, 2)
+multiplyNumber(1, 2)
+divideNumber(1, 2)
